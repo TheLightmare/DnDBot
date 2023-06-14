7f3c8fccbbe3446d3634f1606fd27c9049d11d6b
Mathias Kowalski <slender.kowalski@gmail.com>
2023-06-14 17:46:57 +0300

    starting to work on networking and party management, it will be the base of all the mechanics

--- ui/dnd_ui.py
+++ ui/dnd_ui.py
@@ -6,7 +6,7 @@ from discord.ui import Modal, Button, View, Select, TextInput, UserSelect
 from discord.components import SelectOption
 from settings import *
 import spell
-
+from character import Character
 
 class CampaignUI(View):
     def __init__(self, bot, thread, host: discord.Member):
@@ -17,7 +17,9 @@ class CampaignUI(View):
         self.thread = thread
 
         self.players = [self.host]
-        self.characters = []
+        character = Character(self.host)
+        character.load()
+        self.characters = [character]
 
         invite_player = UserSelect(
             placeholder="Add Player",
@@ -29,11 +31,23 @@ class CampaignUI(View):
         )
         remove_player.callback = self.remove_player
 
+        get_personal_ui = Button(
+            label="Get Personal UI",
+            style=discord.ButtonStyle.blurple,
+        )
+        get_personal_ui.callback = self.get_personal_ui
+
         self.add_item(invite_player)
         self.add_item(remove_player)
+        self.add_item(get_personal_ui)
 
 
     async def invite_player(self, interaction: discord.Interaction):
+        # check if the one who invited is the host
+        if interaction.user != self.host:
+            await interaction.response.send_message("You are not the host", ephemeral=True, delete_after=5)
+            return
+
         player = interaction.data['values'][0]
         # make player into a discord.Member
         player = await self.bot.fetch_user(player)
@@ -46,13 +60,23 @@ class CampaignUI(View):
             await interaction.response.send_message(f"{player.name} is not registered", ephemeral=True, delete_after=5)
             return
         self.players.append(player)
-        character = self.get_character(player)
+        character = Character(player)
+        character.load()
         self.characters.append(character)
 
         # invite the player to the thread
         await self.thread.add_recipient(player)
 
+        # send a message in the thread to the player
+        await self.thread.send(f"{player.mention}, you have joined the game ! You can now use the buttons above to get your personal UI !", delete_after=10)
+
+
     async def remove_player(self, interaction: discord.Interaction):
+        # check if the one who invited is the host
+        if interaction.user != self.host:
+            await interaction.response.send_message("You are not the host", ephemeral=True, delete_after=5)
+            return
+
         player = interaction.data['values'][0]
         # make player into a discord.Member
         player = await self.bot.fetch_user(player)
@@ -62,11 +86,25 @@ class CampaignUI(View):
             await interaction.response.send_message(f"{player.name} is not in the game", ephemeral=True, delete_after=5)
             return
         self.players.remove(player)
-        character = self.get_character(player)
-        self.characters.remove(character)
+
+        self.characters.remove(self.get_character(player))
 
         # invite the player to the thread
         await self.thread.remove_recipient(player)
+
+
+
+    async def get_personal_ui(self, interaction: discord.Interaction):
+        # create the embed
+        embed = discord.Embed(title=f"{interaction.user} Personal UI", description="This is your personal interface", color=0x00ff00)
+        embed.set_author(name=interaction.user.name, icon_url=interaction.user.avatar.url)
+        character = self.get_character(interaction.user)
+        embed.add_field(name="Character", value=f"**Name:** {character.name}\n**Age:** {character.age}", inline=False)
+
+        # send the embed
+        await interaction.response.send_message(embed=embed, view=PlayerUI(interaction.user), ephemeral=True)
+
+
 
 
     def is_registered(self, player_id):
@@ -75,7 +113,13 @@ class CampaignUI(View):
         return str(player_id) in characters
 
     def get_character(self, player):
-        with open(CHARACTER_FOLDER + 'characters.json', 'r') as f:
-            characters = json.load(f)
-        character = characters.get(str(player.id))
-        return character
+        for character in self.characters:
+            if character.author == player:
+                return character
+
+
+class PlayerUI(View):
+    def __init__(self, player):
+        super().__init__(timeout=None)
+
+        self.player = player
