9d1f58b71d4c5330f2e33f95645d55939e716697
Mathias Kowalski <slender.kowalski@gmail.com>
2023-10-31 19:16:08 +0100

    changed everything to slash commands

--- dnd.py
+++ dnd.py
@@ -2,6 +2,7 @@ import json
 
 import discord
 from discord.ext import commands
+from discord import app_commands
 
 from util import misc_utils
 from character import Character
@@ -19,68 +20,69 @@ class Dnd(commands.Cog):
         print('Cog loaded successfully')
 
     #command to register a character
-    @commands.command()
-    async def register(self, ctx : discord.ext.commands.Context):
-        if self.is_registered(ctx.author.id):
-            await ctx.send("You are already registered")
+    @app_commands.command(name="register", description="Register a new character")
+    async def register(self, interaction : discord.Interaction):
+        if self.is_registered(interaction.user.id):
+            await interaction.response.send_message("You are already registered")
             return
 
-        await ctx.send("Starting character creation !", delete_after=5, reference=ctx.message)
+        await interaction.response.send_message("Starting character creation !", delete_after=5)
 
         with open(CHARACTER_FOLDER + 'characters.json', 'r') as f:
             characters = json.load(f)
 
         #create a new discord thread
-        thread = await ctx.channel.create_thread(name="Character creation", reason="Character creation", auto_archive_duration=60)
-        await thread.send(f"{ctx.author.mention} is creating a new character")
+        thread = await interaction.channel.create_thread(name="Character creation", reason="Character creation", auto_archive_duration=60)
+        await thread.send(f"{interaction.user.mention} is creating a new character")
 
-        await misc_utils.create_character(self.bot, thread, ctx.author)
+        await misc_utils.create_character(self.bot, thread, interaction.user)
 
 
 
-    @commands.command()
-    async def unregister(self, ctx):
-        if not self.is_registered(ctx.author.id):
-            await ctx.send("You are not registered")
+    @app_commands.command(name="unregister", description="Unregister your character")
+    async def unregister(self, interaction : discord.Interaction):
+        if not self.is_registered(interaction.user.id):
+            await interaction.response.send_message("You are not registered")
             return
 
-        character = Character(ctx.author)
+        character = Character(interaction.user)
         character.load()
 
         # confirm deletion using buttons
-        deleteconfirmUI = character_ui.DeleteCharacterUI(ctx.author, character)
+        deleteconfirmUI = character_ui.DeleteCharacterUI(interaction.user, character)
 
-        await ctx.send("Are you sure you want to delete your character?", view=deleteconfirmUI)
+        await interaction.response.send_message("Are you sure you want to delete your character?", view=deleteconfirmUI)
 
 
 
-    @commands.command()
-    async def character(self, ctx, user : discord.Member = None):
+    @app_commands.command(name="character", description="View your character sheet")
+    async def character(self, interaction: discord.Interaction, user : discord.Member = None):
         if user is None:
-            user = ctx.author
+            user = interaction.user
         if not self.is_registered(user.id):
-            await ctx.send(f"{user.name} is not registered")
+            await interaction.response.send_message(f"{user.name} is not registered")
             return
 
-        await misc_utils.character_sheet(self.bot, ctx.channel, user)
+        await misc_utils.character_sheet(self.bot, interaction.channel, user)
+        await interaction.response.defer()
 
 
-    @commands.command()
-    async def host(self, ctx):
-        if not self.is_registered(ctx.author.id):
-            await ctx.send("You are not registered", delete_after=5, reference=ctx.message)
+    @app_commands.command(name="host", description="Host a new DnD game")
+    async def host(self, interaction : discord.Interaction):
+        if not self.is_registered(interaction.user.id):
+            await interaction.response.send_message("You are not registered", delete_after=5)
             return
 
-        await ctx.send("Starting a new game ! Hold tight !", delete_after=5, reference=ctx.message)
+        await interaction.response.send_message("Starting a new game ! Hold tight !", delete_after=5)
 
-        character = Character(ctx.author)
+        character = Character(interaction.user)
         character.load()
 
         # create a new discord thread
-        thread = await ctx.channel.create_thread(name=f"{ctx.author}'s Room", reason="DnD")
-        await thread.send(f"{ctx.author.mention} is playing DnD")
+        thread = await interaction.channel.create_thread(name=f"{interaction.user}'s Room", reason="DnD")
+        await thread.send(f"{interaction.user.mention} is playing DnD")
 
-        play = Play(self.bot, thread, ctx.author)
+        play = Play(self.bot, thread, interaction.user)
         await play.start()
 
 
--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-import json
-
 import asyncio
+
 import discord
-from discord.ext import commands, tasks
-from discord.ext.commands import Bot
+from discord import app_commands
+from discord.ext import commands
 
 #==== NECESSARY STUFF ====
 intents = discord.Intents.all()
-TOKEN = "Nzk2NjM2NDc1NjA0NDY3NzEy.GLyZSv.FInt2FjK0lXDH2kWPAzeGkjzsTk7WJ-SG2KJY8"
+TOKEN = ""
 bot = commands.Bot(command_prefix=',', intents=intents)
 
 asyncio.run(bot.load_extension("dnd"))
 @bot.event
 async def on_ready():
-    print('Bot s ready')
+    await bot.tree.sync()
+    print('Praise the code.')
 
 
 bot.run(TOKEN)
